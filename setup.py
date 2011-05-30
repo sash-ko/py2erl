540823cb49c693db7628db1b76ad40e212dc93df
Oleksandr Lysenko <sashkolysenko@gmail.com>
2011-05-30 23:29:49 +0300

    added site url

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author = 'Oleksandr Lysenko',
     author_email = 'sashkolysenko@gmail.com',
     description = ('Primitive and naive Python to Erlang compiler'),
+    url = 'https://github.com/sash-ko/code_samples/tree/master/python/py2erl',
     packages = find_packages(),
     include_package_data=True,
     package_data = {'py2erl': ['erl/*.erl', 'test_files/*.py']},
